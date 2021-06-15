10c553f1b8f6aed5c61f6e2cb08019fda06b1342
tomas berhane <tomib@tomass-MacBook-Air.local>
2021-06-15 09:53:21 +0200

    done

---
+++ app.js
@@ -0,0 +1,12 @@
+
+    function addPlaylist(){
+        let ul = document.getElementById('add-list')
+        let list = document.createElement('li')
+        list.textContent = 'My Playlist'
+        
+        ul.appendChild(list)
+       
+    }
+    addPlaylist()
+   
+
