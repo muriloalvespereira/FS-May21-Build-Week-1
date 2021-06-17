32422cd06ed57280596c0d96b9cf97106660078b
tomas berhane <tomib@tomass-MacBook-Air.local>
2021-06-17 13:48:04 +0200

    added a counter

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-
-    function addPlaylist(){
+let clicks = 0
+   
+   function addPlaylist(){
+    
         let ul = document.getElementById('add-list')
+        clicks ++
         let list = document.createElement('li')
-        list.textContent = 'My Playlist'
+        
+        list.textContent = `My Playlist # ${clicks}`
         
         ul.appendChild(list)
        
