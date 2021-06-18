2347edabfab99213202e8bf7cafd0b4fffee38cc
Murilo Alves <murilo.udi@gmail.com>
2021-06-18 16:35:34 +0200

    Project almost finished

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+let click = 0
 function addPlaylist(){
     let ul = document.getElementById('add-list')
     let list = document.createElement('li')
-    list.textContent = 'My Playlist'
+    click++
+    list.textContent = `My Playlist #${click}`
     
     ul.appendChild(list)
    
